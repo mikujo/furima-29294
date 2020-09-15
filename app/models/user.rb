d856401748ef5b87587c22ca70a7008835f06eb0
mikujo <69742174+mikujo@users.noreply.github.com>
2020-09-15 15:59:07 +0900

    rubocopの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email, format: { with: /[^\s]+@[^\s]+/ }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
     validates :first_name_leading, format: { with: /\A[ァ-ヶー－]+\z/}
