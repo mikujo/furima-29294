d01232d48ec94c09b5cc33cdd763ec72f0cb1dd1
mikujo <69742174+mikujo@users.noreply.github.com>
2020-09-16 14:41:19 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,5 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :detail, :category, :status, :postage, :prefecture, :shipping_day, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :detail, :category_id, :status_id, :postage_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
@@ -8,6 +9,10 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_day
 
-  validates :name, :detail, :category, :status, :postage, :prefecture, :shipping_day, :price, :user, presence: true
-  validates :category, :status, :postage, :prefecture, :shipping_day, numericality: { other_than: 1 }
+  validates :name, :detail, :category, :status, :postage, :prefecture, :shipping_day, :price, presence: true
+  validates :category_id, :status_id, :postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 }
+
+  # 画像必須(ActiveStorage)
+  # price 範囲設定 ¥300~¥9,999,999
+  # price 半角数字のみ
 end
