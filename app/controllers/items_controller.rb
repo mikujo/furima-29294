da96a200204987039272cefafa42cb4ea91af819
mikujo <69742174+mikujo@users.noreply.github.com>
2020-09-21 18:48:14 +0900

    rubocopの実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,9 +49,7 @@ class ItemsController < ApplicationController
 
   def move_to_show
     item = Item.find(params[:id])
-    unless user_signed_in? || current_user.id == item.user_id
-      redirect_to item_path
-    end
+    redirect_to item_path unless user_signed_in? || current_user.id == item.user_id
   end
 
   private
