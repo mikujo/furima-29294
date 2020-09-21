c842d45ae971d61a0571dbf5ad99d7ada3a614f4
mikujo <69742174+mikujo@users.noreply.github.com>
2020-09-21 20:23:21 +0900

    アソシエーションの設定

--- db/migrate/20200921103704_create_orders.rb
+++ db/migrate/20200921103704_create_orders.rb
@@ -4,7 +4,6 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.references :item, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.timestamps
-
     end
   end
 end
