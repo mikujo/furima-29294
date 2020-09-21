162173a91d899bd6c10216aae1d438fc36030fb5
mikujo <69742174+mikujo@users.noreply.github.com>
2020-09-21 14:26:57 +0900

    showへの遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
     redirect_to root_path unless user_signed_in?
   end
