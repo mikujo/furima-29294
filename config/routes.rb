1ac66a5f428f796aae172e67569f2fbc9cd4e38d
mikujo <69742174+mikujo@users.noreply.github.com>
2020-09-22 10:00:09 +0900

    orderへの遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
   resources :items
-  resources :order, only:[:index, :create]
+  resources :orders, only:[:index, :create]
 end
